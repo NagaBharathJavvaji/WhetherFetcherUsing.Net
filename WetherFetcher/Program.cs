7b9505766a10f0e3c4a1c6a88c429f9e5c1266a6
Naga Bharath J <110622665+NagaBharathJavvaji@users.noreply.github.com>
2025-09-14 07:03:05 -0400

    This request is for changes related to building structure of project.

---
+++ WetherFetcher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WetherFetcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Implementation to startup of project
+        }
+    }
+}
